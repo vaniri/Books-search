231d302f192c4f23f7f66de49271efc5a7ef82fa
Vaniri <foxifennec@gmail.com>
2020-03-29 21:16:19 -0400

    prepare for heroku

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('front/build'));
+}
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
